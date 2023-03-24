6ff456eb6b3dbd4d221d9cca59acfbf76cb317eb
RODRIGO PIRES DA SILVA <46466752+digonad4@users.noreply.github.com>
2023-03-24 11:13:13 -0300

    Update MiEasyRecovery (pt_br).bat

--- MiEasyRecovery (pt_br).bat
+++ MiEasyRecovery (pt_br).bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFClbSBaLO2SGIroL5uT07u6Unn4YWaIr20LPz7iBbbFd40brFQ==
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZksaHGQ=
-::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbAs=
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATE1FA/LAldSwuMPW/uZg==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFClbSBaLO2SGIrAP4/z0/9aqqw04VfEmOL3e2buYJfIKqg30YakvxDMa1s4UCXs=
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 echo off
 color 03
 mode 72,15
